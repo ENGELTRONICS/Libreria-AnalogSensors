3a7e0be48c2abb3712151ae74b31ba7cbfc5b7e3
FELIPE ANDRES ANGEL RODRIGUEZ <felipeandresangelrodriguez@MacBook-Pro-de-FELIPE.local>
2020-07-30 02:11:37 -0500

    Añadió comentarios en el encabezado del archivo .h sobre la lícencia y la versión de la librería.

--- AnalogSensors.h
+++ AnalogSensors.h
@@ -1,8 +1,32 @@
+/******************************************************************************************************************
+  MIT License
+
+  Copyright (c) 2020 ENGELTRONICS
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+******************************************************************************************************************/
 /******************************************** Project information *************************************************
   Name Proyect: Librería AnalogSensors
         Author: Ing.Felipe Andrés Ángel Rodríguez
-          Data: 10/Julio/2020
-           Rev: 1.0
+          Data: 30/Julio/2020
+           Rev: 1.0.0
 
   Description : Esta librería se encarga de leer los valores de un sensor analógico, también se implementan
                 diferentes filtros para el procesamiento de datos.
